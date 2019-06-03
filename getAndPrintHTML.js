611d1bcf7893b122ac19dae4c7f20c5a568f28f8
Steven Wing <steven.wing82@gmail.com>
2019-06-03 18:23:46 +0000

    completed step 2

--- getAndPrintHTML.js
+++ getAndPrintHTML.js
@@ -1,16 +1,21 @@
 const https = require('https');
 
-
 function getAndPrintHTML() {
+    let requestOptions = {
+        host: 'sytantris.github.io',
+        path: 'http-examples/step2.html'
+    }
+    let buffer = '';
     https.get(requestOptions, function(response) {
         response.setEncoding('utf8');
-        let requestOptions = {
-            host: 'sytantris.github.io',
-            path: 'http-examples/step2.html'
-        }
+        response.on('data', function(data) {
+            buffer += '\n' + data;
+            chunks++;
+        })
 
-
-
+        response.on('end', function() {
+            console.log(buffer);
+        })
     })
 
 }
